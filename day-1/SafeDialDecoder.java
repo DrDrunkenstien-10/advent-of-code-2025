2cb6032f6f69d99f91567c6951178e92bae1eac0
Atharva Jadhav <drdrunkenstien10@gmail.com>
2025-12-01 23:38:51 +0530

    Day 1, Part 1

---
+++ day-1/SafeDialDecoder.java
@@ -0,0 +1,93 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SafeDialDecoder {
+	public static int decodeSafeDial(String[] input) {
+		int count = 0;
+		int dialPosition = 50;
+
+		for(int i = 0; i < input.length; i++) {
+			String currentRotation = input[i];
+			
+			int first = 0; // First number in the string (e.g., 6 in L68)
+		        int second = 0; // Second number in the string (e.g., 8 in L68)
+			int third = 0; // Third number in the string (e.g., 1 in L951)
+
+			int addition = 0;
+			
+			if (currentRotation.length() == 4) {
+                                // In case of string like R251
+                                first = Integer.parseInt(String.valueOf(currentRotation.charAt(1))) * 100;
+                                second = Integer.parseInt(String.valueOf(currentRotation.charAt(2))) * 10;
+                                third = Integer.parseInt(String.valueOf(currentRotation.charAt(3)));
+
+                                addition = first + second + third;
+                        }
+
+			else if(currentRotation.length() == 3) {
+				// In case of string like R25
+				first = Integer.parseInt(String.valueOf(currentRotation.charAt(1))) * 10;
+				second = Integer.parseInt(String.valueOf(currentRotation.charAt(2)));
+
+				addition = first + second;
+			}
+			
+			else {
+				// In case of string like R2
+				first = Integer.parseInt(String.valueOf(currentRotation.charAt(1)));
+				addition = first;
+			}
+
+			if(currentRotation.charAt(0) == 'L') {
+				dialPosition = Math.floorMod(dialPosition - addition, 100);
+			    	
+				if(dialPosition == 0) {
+					count++;
+				}
+			}
+
+			else {
+				dialPosition = Math.floorMod(dialPosition + addition, 100);
+
+				if(dialPosition == 0) {
+					count++;
+				}
+			}
+		}
+		
+		return count;
+	}
+	
+	public static String[] readInputFromFile(String fileName) {
+		List<String> inputList = new ArrayList<>();
+			
+		File file = new File(fileName);
+
+		try (Scanner scanner = new Scanner(file)) {
+			while(scanner.hasNextLine()) {
+				String input = scanner.nextLine();
+				inputList.add(input);
+			}
+		}
+
+		catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+      			e.printStackTrace();
+		}
+
+		String[] inputArray = inputList.toArray(new String[0]);
+
+		return inputArray;
+	}
+
+	public static void main(String[] args) {
+		String[] input = readInputFromFile("safe-dial-decoder-input.txt");
+
+		int password = decodeSafeDial(input);
+		System.out.println("Password: " + password);
+	}
+}
