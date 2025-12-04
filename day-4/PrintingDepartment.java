dce67a7430a20ad6c15b11b069237e7dcc1ba1c8
Atharva Jadhav <drdrunkenstien10@gmail.com>
2025-12-04 14:57:44 +0530

    Day 4, Part 2

--- day-4/PrintingDepartment.java
+++ day-4/PrintingDepartment.java
@@ -6,37 +6,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrintingDepartment {
+	// Part 1
 	public static int findAccessiblePaperRolls(String[] input) {
 		int count = 0;
 
 		int rows = input.length;
 		int columns = input[0].length();
-		
+
 		// All the 8 directions
 		int[][] directions = {
-			{-1, -1}, {-1, 0}, {-1, 1},
-			{0, -1},           {0, 1},
-			{1, -1},  {1, 0},  {1, 1}
+				{ -1, -1 }, { -1, 0 }, { -1, 1 },
+				{ 0, -1 }, { 0, 1 },
+				{ 1, -1 }, { 1, 0 }, { 1, 1 }
 		};
 
-		for(int i = 0; i < rows; i++) {
-			for(int j = 0; j < columns; j++) {
-				if(input[i].charAt(j) == '@') {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				if (input[i].charAt(j) == '@') {
 					int adjacent = 0;
 
-					for(int[] d : directions) {
+					for (int[] d : directions) {
 						int nextRow = i + d[0];
 						int nextColumn = j + d[1];
 
-						if(nextRow >= 0 && nextRow < rows && nextColumn >= 0 && nextColumn < columns) {
-							if(input[nextRow].charAt(nextColumn) == '@') {
+						if (nextRow >= 0 && nextRow < rows && nextColumn >= 0 && nextColumn < columns) {
+							if (input[nextRow].charAt(nextColumn) == '@') {
 								adjacent++;
 							}
 						}
 
 					}
 
-					if(adjacent < 4) {
+					if (adjacent < 4) {
 						count++;
 					}
 				}
@@ -46,13 +47,72 @@ public class PrintingDepartment {
 		return count;
 	}
 	
+	// Part 2
+	public static int findTotalRemovedPaperRolls(String[] input) {
+		int totalRemoved = 0;
+
+		int rows = input.length;
+		int columns = input[0].length();
+		
+		char[][] grid = new char[rows][columns];
+
+		for(int i = 0; i < rows; i++) {
+			grid[i] = input[i].toCharArray();
+		}
+		
+		int[][] directions = {
+        			{-1, -1}, {-1, 0}, {-1, 1},
+        			{0, -1},           {0, 1},
+        			{1, -1},  {1, 0},  {1, 1}
+		};
+
+		while(true) {
+			List<int[]> toRemove = new ArrayList<>();
+
+			for(int i = 0; i < rows; i++) {
+				for(int j = 0; j < columns; j++) {
+					if(grid[i][j] == '@') {
+						int adjacent = 0;
+						
+						for(int[] d : directions) {
+							int nextRow = i + d[0];
+							int nextColumn = j + d[1];
+
+							if(nextRow >= 0 && nextRow < rows && nextColumn >= 0 && nextColumn < columns) {
+								if(grid[nextRow][nextColumn] == '@') {
+									adjacent++;
+								}
+							}
+						}
+
+						if(adjacent < 4) {
+							toRemove.add(new int[]{i, j});
+						}
+					}
+
+				}
+			}
+
+			if(toRemove.isEmpty()) {
+				break;
+			}
+
+			for(int[] pos : toRemove) {
+				grid[pos[0]][pos[1]] = '.';
+				totalRemoved++;
+			}
+		}
+
+		return totalRemoved;
+	}
+		
 	public static String[] readInputFromFile(String fileName) {
 		List<String> inputList = new ArrayList<>();
 
 		File file = new File(fileName);
 
 		try (Scanner scanner = new Scanner(file)) {
-			while(scanner.hasNextLine()) {
+			while (scanner.hasNextLine()) {
 				String input = scanner.nextLine();
 				inputList.add(input);
 			}
@@ -60,7 +120,7 @@ public class PrintingDepartment {
 
 		catch (FileNotFoundException e) {
 			System.out.println("An error occurred.");
-      			e.printStackTrace();
+			e.printStackTrace();
 		}
 
 		String[] inputArray = inputList.toArray(new String[0]);
@@ -69,24 +129,27 @@ public class PrintingDepartment {
 	}
 
 	public static void main(String[] args) {
-		/*
+		/* 
 		String[] input = {
-			"..@@.@@@@.",
-            		"@@@.@.@.@@",
-            		"@@@@@.@.@@",
-            		"@.@@@@..@.",
-            		"@@.@@@@.@@",
-            		".@@@@@@@.@",
-            		".@.@.@.@@@",
-            		"@.@@@.@@@@",
-            		".@@@@@@@@.",
-            		"@.@.@@@.@."
-		};
-		*/
-		
+		 "..@@.@@@@.",
+		 "@@@.@.@.@@",
+		 "@@@@@.@.@@",
+		 "@.@@@@..@.",
+		 "@@.@@@@.@@",
+		 ".@@@@@@@.@",
+		 ".@.@.@.@@@",
+		 "@.@@@.@@@@",
+		 ".@@@@@@@@.",
+		 "@.@.@@@.@."
+		 };
+		 */
+
 		String[] input = readInputFromFile("printing-department-input.txt");
 
 		int count = findAccessiblePaperRolls(input);
 		System.out.println("Accessible Paper Rolls: " + count);
+
+		int totalRemoved = findTotalRemovedPaperRolls(input);
+		System.out.println("Total Removed Paper Rolls: " + totalRemoved);
 	}
 }
